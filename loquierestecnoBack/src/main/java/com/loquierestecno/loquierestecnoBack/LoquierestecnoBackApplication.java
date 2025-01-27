1166d4cc7e41f89f8c4c45e4531c404117e92498
aeduardomartinez <andres.ok30@hotmail.com>
2025-01-27 16:04:53 -0500

    Proyecto back tienda virtual

---
+++ loquierestecnoBack/src/main/java/com/loquierestecno/loquierestecnoBack/LoquierestecnoBackApplication.java
@@ -0,0 +1,13 @@
+package com.loquierestecno.loquierestecnoBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoquierestecnoBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoquierestecnoBackApplication.class, args);
+	}
+
+}
---
+++ loquierestecnoBack/src/test/java/com/loquierestecno/loquierestecnoBack/LoquierestecnoBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loquierestecno.loquierestecnoBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoquierestecnoBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
